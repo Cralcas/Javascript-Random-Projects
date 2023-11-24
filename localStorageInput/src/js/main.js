6c5639c0be63c9e664338b297883e0cf7a78db97
Cralcas <carlosbergq@gmail.com>
2023-11-24 20:26:03 +0100

    "first commit"

---
+++ localStorageInput/src/js/main.js
@@ -0,0 +1,34 @@
+import "./../css/style.css";
+
+// let input = document.querySelector("input"),
+//   h2 = document.querySelector("h2");
+
+// h2.innerHTML = localStorage.getItem("completed");
+
+// input.addEventListener("keyup", () => {
+//   localStorage.setItem("completed", input.value);
+//   h2.innerHTML = localStorage.getItem("completed");
+// });
+
+const theList = document.getElementById("theList");
+
+let todos = [];
+
+const addTodo = (e) => {
+  e.preventDefault();
+  let todo = {
+    task: document.getElementById("todoInput").value,
+  };
+  todos.push(todo);
+  document.querySelector("form").reset();
+
+  let listItem = document.createElement("li");
+  listItem.innerHTML = todo.task;
+  theList.appendChild(listItem);
+
+  localStorage.setItem("completed", JSON.stringify(todos));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("btn").addEventListener("click", addTodo);
+});
